f87c1532d0b62d84c3a46551e175358f93b0f826
masab-m <masabm263@gmail.com>
2023-01-07 21:08:40 +0500

    Stats

--- src/components/Stats/Stats.vue
+++ src/components/Stats/Stats.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-stats
-    </div>
+    <b-container fluid>
+        <b-row>
+            <b-row>
+                <b-col>
+                    <div class="headText">
+                        <h3>Stats</h3>
+                    </div>
+                </b-col>
+            </b-row>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
