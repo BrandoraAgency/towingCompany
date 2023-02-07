e727701f7b2c7219e88ccc6385b5957f69c20b49
masab-m <masabm263@gmail.com>
2023-02-07 15:04:29 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const rollupConfig = {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
-  plugins: [
-    // Add other Rollup plugins here, if necessary
-  ],
-  external: [
-    'pdf-lib'
-  ]
-};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  rollup: rollupConfig
+  external: [
+    'pdf-lib'
+  ]
 })
