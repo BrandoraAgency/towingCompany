c8849916f02ecac4e1776af841622db84c97f1d6
Jamal-logic <jamal.massab3@gmail.com>
2024-02-28 13:58:05 +0500

    adding upsell column as well

--- src/components/Job/JobList.vue
+++ src/components/Job/JobList.vue
@@ -143,6 +143,7 @@
                         <b-th>Amount</b-th>
                         <b-th>GOA</b-th>
                         <b-th>Trade Name</b-th>
+                        <b-th>UpSell</b-th>
                         <b-th>Charged</b-th>
                         <b-th>State</b-th>
                         <b-th>Miles</b-th>
@@ -171,6 +172,7 @@
                         <b-td>${{ job.amount }}</b-td>
                         <b-td>{{ job.jobStatus==='goa' ? '$'+job.amount :'' }} </b-td>
                         <b-td>{{ job.Trade_Name? job.Trade_Name:'' }}</b-td>
+                        <b-td>{{ job.upSellAmount? '$'+job.upSellAmount:'' }}</b-td>
                         <b-td> <span class="tstatus">{{ job.jobStatus }}</span>  </b-td>
                         <b-td>{{ job.state }}</b-td>
                         <b-td>{{ job.miles }}</b-td>
